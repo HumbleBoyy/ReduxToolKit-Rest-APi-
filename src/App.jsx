8afa1755dd5b603121abb49c6b0c78282c731233
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-02-20 09:43:52 +0500

    New Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import getRequest from './service/getRequest'
 
 const App = () => {
+  const products = getRequest("/products")
+  console.log(products)
   return (
     <div className='flex justify-center h-[100vh] items-center'>
       App
