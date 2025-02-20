54a9692d11787a24ea16741f605120c0b146d6f4
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-02-20 10:15:46 +0500

    New Commit

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import getRequest from '../service/getRequest'
+import ProductCard from '../components/ProductCard'
 
 const Products = () => {
+    const {products} = getRequest("/products")
+    console.log(products)
   return (
     <div>
-      Hello
+      {products?.map(item => <ProductCard key={item.id} item={item}/>)}
     </div>
   )
 }
