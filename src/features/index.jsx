7309f7683d200555e318493fd6dd69b165d50248
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-02-20 09:58:07 +0500

    new Commit

--- src/features/index.jsx
+++ src/features/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from '../modules/Navbar'
+import Products from '../pages/Products'
 
-const Layout = ({children}) => {
+const Layout = () => {
   return (
     <div className='flex flex-col'>
       <Navbar/>
-      {children}
+      <Products/>
     </div>
   )
 }
